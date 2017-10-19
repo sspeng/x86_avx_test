2809e9d9a67e2207ef37f820fcca3975277fa561
石树鹏 <shishupeng@mail.nsccwx.cn>
2017-10-19 10:55:46 +0800

    初步向量化并没有充分填充向量指令的流水线，即没有掩盖浮点计算的延迟，版本2通过循环展开的技术，以使用更多的寄存器，来解除数据依赖，填充数据计算流水线

--- pi.c
+++ pi.c
@@ -17,7 +17,7 @@ double compute_pi_native(size_t dt)
 
 	for(i = 0; i < dt; i++)
 	{
-		double x = (double) i/ dt;
+		double x = (double) i / dt;
 		pi += delta / (1.0 + x * x);
 	}
 	return pi * 4.0;
@@ -28,6 +28,7 @@ double compute_pi_omp_avx(size_t dt)
 	int i;
 	double pi = 0.0;
 	double delta = 1.0/ dt;
+
 	__m256d ymm0, ymm1, ymm2, ymm3, ymm4;
 	ymm0 = _mm256_set1_pd(1.0);
 	ymm1 = _mm256_set1_pd(delta);
@@ -49,6 +50,44 @@ double compute_pi_omp_avx(size_t dt)
 
 	return pi * 4.0;
 }
+ 
+double compute_pi_omp_avx_v2(size_t dt)
+{
+	int i;
+	double pi = 0.0;
+	double delta = 1.0/ dt;
+
+	__m256d ymm0, ymm1, ymm2, ymm3, ymm4, ymm5, ymm6;
+	ymm0 = _mm256_set1_pd(1.0);
+	ymm1 = _mm256_set1_pd(delta);
+	ymm2 = _mm256_set_pd(delta * 3, delta * 2, delta, 0.0);
+	ymm4 = _mm256_setzero_pd();
+	ymm5 = _mm256_setzero_pd();
+
+	for(i = 0; i <= dt - 8; i += 8)
+	{
+		ymm3 = _mm256_set1_pd(i * delta);
+		ymm3 = _mm256_add_pd(ymm3, ymm2);
+		ymm3 = _mm256_mul_pd(ymm3, ymm3);
+		ymm3 = _mm256_add_pd(ymm0, ymm3);
+		ymm3 = _mm256_div_pd(ymm1, ymm3);
+		ymm4 = _mm256_add_pd(ymm4, ymm3);
+
+		ymm6 = _mm256_set1_pd((i + 4) * delta);
+		ymm6 = _mm256_add_pd(ymm6, ymm2);
+		ymm6 = _mm256_mul_pd(ymm6, ymm6);
+		ymm6 = _mm256_add_pd(ymm0, ymm6);
+		ymm6 = _mm256_div_pd(ymm1, ymm6);
+		ymm5 = _mm256_add_pd(ymm5, ymm6);
+	}
+	ymm4 = _mm256_add_pd(ymm4, ymm5);
+	double tmp[4] __attribute__((aligned(32)));
+	_mm256_store_pd(tmp, ymm4);
+	pi += tmp[0] + tmp[1] + tmp[2] + tmp[3];
+
+	return pi * 4.0;
+}
+
 int main()
 {
 	double t1, t2;
@@ -60,11 +99,19 @@ int main()
 	get_time(t2);
 	printf("pi = %lf\n", pi);
 	printf("time = %lfs\n", t2 - t1);
-	printf("**********end**********\n");
+	printf("**********end**********\n\n");
 
 	printf("**********avx**********\n");
 	get_time(t1);
 	pi = compute_pi_omp_avx(dt);
+	get_time(t2);
+	printf("pi = %lf\n", pi);
+	printf("time = %lfs\n", t2 - t1);
+	printf("**********end**********\n\n");
+
+	printf("**********avx v2*******\n");
+	get_time(t1);
+	pi = compute_pi_omp_avx_v2(dt);
 	get_time(t2);
 	printf("pi = %lf\n", pi);
 	printf("time = %lfs\n", t2 - t1);
